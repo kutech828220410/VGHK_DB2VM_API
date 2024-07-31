e4cbb526ca1d7d15cad2638b9e1b1667b0cac2ff
hsonds01 <hsonds01@HTS81P>
2024-07-31 15:49:13 +0800

    20240731

--- Tohs.cs
+++ Tohs.cs
@@ -16,6 +16,7 @@ using System.Net.Http.Headers;
 using System.IO;
 using HIS_DB_Lib;
 using System.Data.SqlClient;
+using MyOffice;
 namespace DB2VM_API
 {
     [Route("[controller]")]
@@ -51,6 +52,28 @@ namespace DB2VM_API
             }
 
         }
+        public class cdmis_class
+        {
+            public string 藥碼 = "";
+            public string 藥名 = "";
+            public string 藥局 = "";
+            public string 交易時間 = "";
+            public string 收支原因 = "";
+            public string 收入數量 = "";
+            public string 數量 = "";
+            public string 收入藥品批號_效期 = "";
+            public string 支出數量 = "";
+            public string 結存數量 = "";
+            public string 姓名 = "";
+            public string 調配藥師字號 = "";
+            public string 調配藥師 = "";
+            public string 點斑藥師 = "";
+            public string 核對藥師 = "";
+            public string 病歷號 = "";
+            public string 備註 = "";
+        }
+
+
         [Route("download/ITUD0031")]
         [HttpGet]
         public async Task<ActionResult> GET_download_ITUD0031()
@@ -212,6 +235,163 @@ namespace DB2VM_API
                 return ex.Message;
             }
             return str;
+        }
+
+        [Route("download_excel/{value}")]
+        [HttpGet]
+        public async Task<ActionResult> GET_download_excel(string value)
+        {
+            try
+            {
+                string[] str_ary = value.Split(',');
+                if (str_ary.Length != 2)
+                {
+                    return StatusCode(500, "傳入日期格式錯誤");
+                }
+                if (str_ary[0].Length != 12 || str_ary[1].Length != 12)
+                {
+                    return StatusCode(500, "傳入日期格式錯誤");
+                }
+                string date1 = $"{str_ary[0].Substring(0, 4)}-{str_ary[0].Substring(4, 2)}-{str_ary[0].Substring(6, 2)} {str_ary[0].Substring(8, 2)}:{str_ary[0].Substring(10, 2)}:00";
+                string date2 = $"{str_ary[1].Substring(0, 4)}-{str_ary[1].Substring(4, 2)}-{str_ary[1].Substring(6, 2)} {str_ary[1].Substring(8, 2)}:{str_ary[1].Substring(10, 2)}:00";
+                if(date1.Check_Date_String() == false || date2.Check_Date_String() == false)
+                {
+                    return StatusCode(500, "傳入日期格式錯誤");
+                }
+
+                string connectionString = "Server=192.168.222.19;Database=CabinetDB_new;User Id=Cabinet_new;Password=Cabinet_new;";
+                string query = $"SELECT * FROM TransLog WHERE (MedicalRecordNo != '取消交易' AND MedicalRecordNo != '誤開抽屜' AND MedicalRecordNo != '修改藥名' AND MedicalRecordNo != '')AND (TransTime >= '{date1}' AND TransTime <= '{date2}');";
+                List<cdmis_class> cdmis_Classes = new List<cdmis_class>();
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("連接成功!");
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string 藥碼 = reader["DrugCode"].ToString();
+                                string 藥名 = reader["DrugName"].ToString();
+                                string 藥局 = reader["CabinetName"].ToString();
+                                if (藥局 == "門診藥局管2") 藥局 = "ER";
+                                if (藥局 == "門診藥局管3") 藥局 = "OPD";
+
+                                DateTime 交易時間 = reader["TransTime"].ToDateTimeString().StringToDateTime();
+                                string 收支原因 = reader["MedicalRecordNo"].ToString();
+                                string 數量 = reader["TransAmount"].ToString();
+                                string 結存數量 = reader["Balance"].ToString();
+                                string 藥師姓名_temp = reader["UserName"].ToString();
+                                string 藥師姓名 = 藥師姓名_temp;
+                                string 調配藥師字號 = "";
+                                string 核對藥師姓名 = reader["AuditorName"].ToString();
+
+                                string 藥師ID = reader["UserNo"].ToString();
+                                string 病歷號 = "";
+
+                                if(收支原因 != "領取藥品" && 收支原因 != "對點作業" && 收支原因 != "存入藥品" && 收支原因 != "對點校正")
+                                {
+                                    病歷號 = reader["MedicalRecordNo"].ToString().Trim();
+                                    收支原因 = "調劑領藥";
+                                }
+
+                                if (藥師姓名_temp.Length >= 9)
+                                {                              
+                                    藥師姓名 = 藥師姓名_temp.Substring(6, 3);
+                                    調配藥師字號 = 藥師姓名_temp.Substring(0, 6);
+                                }
+                                cdmis_class cdmis_Class = new cdmis_class();
+                                cdmis_Class.藥碼 = 藥碼;
+                                cdmis_Class.藥名 = 藥名;
+                                cdmis_Class.藥局 = 藥局;
+                                cdmis_Class.交易時間 = 交易時間.ToDateTimeString();
+                                cdmis_Class.收支原因 = 收支原因;
+                                cdmis_Class.病歷號 = 病歷號;
+                                cdmis_Class.調配藥師字號 = 調配藥師字號;
+                                cdmis_Class.結存數量 = 結存數量;
+                                if (收支原因 == "領取藥品")
+                                {
+                                    cdmis_Class.支出數量 = 數量;
+                                    cdmis_Class.調配藥師 = 藥師姓名;
+                                    cdmis_Class.調配藥師字號 = 調配藥師字號;
+                                }
+                                else if (收支原因 == "對點作業" || 收支原因 == "對點校正")
+                                {
+                                    cdmis_Class.點斑藥師 = 藥師姓名;
+                                    cdmis_Class.核對藥師 = 核對藥師姓名;
+                                }
+                                else if (收支原因 == "存入藥品")
+                                {
+                                    cdmis_Class.收入數量 = 數量;
+                                    cdmis_Class.調配藥師 = 藥師姓名;
+                                    cdmis_Class.調配藥師字號 = 調配藥師字號;
+                                }
+                                else if (收支原因 == "調劑領藥")
+                                {
+                                    cdmis_Class.支出數量 = 數量;
+                                    cdmis_Class.調配藥師 = 藥師姓名;
+                                    cdmis_Class.調配藥師字號 = 調配藥師字號;
+                                }
+                                else
+                                {
+
+                                }
+                                cdmis_Class.備註 = 藥局;
+                                cdmis_Classes.Add(cdmis_Class);
+                            }
+                        }
+                    }
+                
+                }
+
+                string loadText = Basic.MyFileStream.LoadFileAllText(@"./cmdi_excel.txt", "utf-8");
+                int row_max = 60000;
+                string xlsx_command = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                string xls_command = "application/vnd.ms-excel";
+
+                List<SheetClass> sheetClasses = new List<SheetClass>();
+
+                List<cdmis_class> cdmis_Classes_buf = new List<cdmis_class>();
+                List<string> Codes = (from temp in cdmis_Classes
+                                      select temp.藥碼).Distinct().ToList();
+                for (int i = 0; i < Codes.Count; i++)
+                {
+                    cdmis_Classes_buf = (from temp in cdmis_Classes
+                                         where temp.藥碼 == Codes[i]
+                                         select temp).ToList();
+                    SheetClass sheetClass = loadText.JsonDeserializet<SheetClass>();
+                    sheetClass.Name = $"{Codes[i]}";
+                    sheetClass.ReplaceCell(1, 1, $"{cdmis_Classes_buf[0].藥名}");
+                    sheetClass.ReplaceCell(1, 4, $"{cdmis_Classes_buf[0].藥名}");
+                    for (int k = 0; k < cdmis_Classes_buf.Count; k++)
+                    {
+                        sheetClass.AddNewCell_Webapi(8 + k, 0, $"{cdmis_Classes_buf[k].交易時間}", "微軟正黑體", 12, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                        sheetClass.AddNewCell_Webapi(8 + k, 1, $"{cdmis_Classes_buf[k].收支原因}", "微軟正黑體", 14, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                        sheetClass.AddNewCell_Webapi(8 + k, 2, $"{cdmis_Classes_buf[k].收入數量}", "微軟正黑體", 14, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                        sheetClass.AddNewCell_Webapi(8 + k, 3, $"{cdmis_Classes_buf[k].收入藥品批號_效期}", "微軟正黑體", 14, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                        sheetClass.AddNewCell_Webapi(8 + k, 4, $"{cdmis_Classes_buf[k].支出數量}", "微軟正黑體", 14, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                        sheetClass.AddNewCell_Webapi(8 + k, 5, $"{cdmis_Classes_buf[k].結存數量}", "微軟正黑體", 14, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                        sheetClass.AddNewCell_Webapi(8 + k, 6, $"{cdmis_Classes_buf[k].病歷號}", "微軟正黑體", 14, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                        sheetClass.AddNewCell_Webapi(8 + k, 7, $"{cdmis_Classes_buf[k].調配藥師}", "微軟正黑體", 14, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                        sheetClass.AddNewCell_Webapi(8 + k, 8, $"{cdmis_Classes_buf[k].調配藥師字號}", "微軟正黑體", 14, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                        sheetClass.AddNewCell_Webapi(8 + k, 9, $"{cdmis_Classes_buf[k].點斑藥師}", "微軟正黑體", 14, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                        sheetClass.AddNewCell_Webapi(8 + k, 10, $"{cdmis_Classes_buf[k].核對藥師}", "微軟正黑體", 14, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                        sheetClass.AddNewCell_Webapi(8 + k, 11, $"{cdmis_Classes_buf[k].備註}", "微軟正黑體", 14, false, NPOI_Color.BLACK, 430, NPOI.SS.UserModel.HorizontalAlignment.Left, NPOI.SS.UserModel.VerticalAlignment.Bottom, NPOI.SS.UserModel.BorderStyle.Thin);
+                    }
+                    sheetClasses.Add(sheetClass);
+                }
+
+                // 構建文件路徑
+                byte[] excelData = sheetClasses.NPOI_GetBytes(Excel_Type.xls);
+                Stream stream = new MemoryStream(excelData);
+                return await Task.FromResult(File(stream, xls_command, $"{DateTime.Now.ToDateString("-")}_收支結存簿冊.xls"));
+
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "內部伺服器錯誤");
+            }
         }
     }
 }
--- batch_TohsCopy/Program.cs
+++ batch_TohsCopy/Program.cs
@@ -7,19 +7,80 @@ using Basic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.IO;
+using System.Reflection;
+
 namespace batch_TohsCopy
 {
     class Program
     {
+        static public string currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        static private string MyConfigFileName = $"{currentDirectory}//MyConfig.txt";
+        public class MyConfigClass
+        {
+            private string _improtFilename = "D:\\Tohs\\Import\\TOHS\\importcabinet.csv";
+            private string _exprotFilename = "D:\\Tohs\\Import\\TOHS\\exportcabinet.csv";
+
+
+            private bool creat_importcabinet = false;
+            private bool creat_exportcabinet = false;
+
+            public bool Creat_importcabinet { get => creat_importcabinet; set => creat_importcabinet = value; }
+            public bool Creat_exportcabinet { get => creat_exportcabinet; set => creat_exportcabinet = value; }
+            public string ImprotFilename { get => _improtFilename; set => _improtFilename = value; }
+            public string ExprotFilename { get => _exprotFilename; set => _exprotFilename = value; }
+        }
+        static MyConfigClass myConfigClass = new MyConfigClass();
+        static public bool LoadDBConfig()
+        {
+            string jsonstr = MyFileStream.LoadFileAllText($"{MyConfigFileName}");
+            Console.WriteLine($"路徑 : {MyConfigFileName} 開始讀取....");
+            if (jsonstr.StringIsEmpty())
+            {
+
+                jsonstr = Basic.Net.JsonSerializationt<MyConfigClass>(new MyConfigClass(), true);
+                List<string> list_jsonstring = new List<string>();
+                list_jsonstring.Add(jsonstr);
+                if (!MyFileStream.SaveFile($"{MyConfigFileName}", list_jsonstring))
+                {
+                    Console.WriteLine($"建立{MyConfigFileName}檔案失敗!");
+                    return false;
+                }
+                Console.WriteLine($"未建立參數文件!請至子目錄設定{MyConfigFileName}");
+                return false;
+            }
+            else
+            {
+                myConfigClass = Basic.Net.JsonDeserializet<MyConfigClass>(jsonstr);
+
+                jsonstr = Basic.Net.JsonSerializationt<MyConfigClass>(myConfigClass, true);
+                List<string> list_jsonstring = new List<string>();
+                list_jsonstring.Add(jsonstr);
+                if (!MyFileStream.SaveFile($"{MyConfigFileName}", list_jsonstring))
+                {
+                    Console.WriteLine($"建立{MyConfigFileName}檔案失敗!");
+                    return false;
+                }
+
+            }
+            return true;
+
+        }
         async static Task Main(string[] args)
         {
             try
             {
-                await DownloadFileAsync("http://10.18.1.146:4435/Tohs/download/ITUD0031", @"C:\\data\\importcabinet.csv");
-                Logger.Log($"[DownloadFileAsync] http://10.18.1.146:4435/Tohs/download/ITUD0031    C:\\data\\importcabinet.csv  ....ok");
-                await DownloadFileAsync("http://10.18.1.146:4435/Tohs/download/UDMORQRE", @"C:\\data\\cabinet.csv");
-                Logger.Log($"[DownloadFileAsync] http://10.18.1.146:4435/Tohs/download/UDMORQRE    C:\\data\\cabinet.csv  ....ok");
-
+                if (LoadDBConfig() == false) return;
+                if(myConfigClass.Creat_importcabinet)
+                {
+                    await DownloadFileAsync("http://10.18.1.146:4435/Tohs/download/ITUD0031", $@"{myConfigClass.ImprotFilename}");
+                    Logger.Log($"[DownloadFileAsync] http://10.18.1.146:4435/Tohs/download/ITUD0031    {myConfigClass.ImprotFilename}  ....ok");
+                }
+                if (myConfigClass.Creat_exportcabinet)
+                {
+                    await DownloadFileAsync("http://10.18.1.146:4435/Tohs/download/UDMORQRE", $@"{myConfigClass.ExprotFilename}");
+                    Logger.Log($"[DownloadFileAsync] http://10.18.1.146:4435/Tohs/download/UDMORQRE    {myConfigClass.ExprotFilename}  ....ok");
+                }
+                 
             }
             catch (Exception ex)
             {
